79d49a871906b44bf5252e3dc5de82d48c81cb7e
Ethan <elnair122@gmail.com>
2023-04-04 15:48:36 -0400

    django installed and project crated

---
+++ video1/notes.py
@@ -0,0 +1,23 @@
+# Django tutorial - Setup, Installation, and Page Navigation
+
+# video 1 is about setting up project (installation), how Django works, getting the server running, 
+# Other videos are about databases, templates, etc. 
+
+
+# Django is a fullstack web framework that allows you to create websites with pure Python and a little bit of html
+# This is great because typically when you make websites, you need a combination of Javascript, HTML, CSS, PHP, etc.
+    # if you know Python, Django is a good way to create nice and fully functional websites
+
+
+
+# Installation
+    # command: pip3 install django
+    # if you hit enter and doesn't work, he has a video for what to do
+
+# Getting started
+    # he is working in a virtual environment, so he does the command "activate django"
+
+    # to create a project, do the command: django-admin startproject name_of_project 
+            # - you can't name the project anything that is a python package, like numpi or pygame
+            # - what I did, is cd to video1 directory, and did: django-admin startproject video1project
+
